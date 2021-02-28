d8cb69490badb2f8cc8d8e45fe7cf04a1d8d6b1c
bagus2x <dutapanggulu@gmail.com>
2021-03-01 06:58:03 +0700

    remove godotenv

--- api/app.go
+++ api/app.go
@@ -18,7 +18,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/recover""
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
